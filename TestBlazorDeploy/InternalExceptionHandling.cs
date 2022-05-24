5bba81d04ca67b365a65ec382e87485e0019662a
Kasbolat Kumakhov <uhfath@gmail.com>
2022-05-24 16:51:07 +0300

    Add project files.

---
+++ TestBlazorDeploy/InternalExceptionHandling.cs
@@ -0,0 +1,14 @@
+﻿namespace TestBlazorDeploy
+{
+	public static class InternalExceptionHandling
+	{
+		public static void Init()
+		{
+			AppDomain.CurrentDomain.FirstChanceException += (s, e) =>
+			{
+				Console.WriteLine("FIRST_CHANCE");
+			};
+			Console.WriteLine("INITIALIZED");
+		}
+	}
+}
